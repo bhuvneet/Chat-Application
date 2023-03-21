181093227fc15ea0b6c83348db06ce70bdb467ef
bhuvneet <bhuvneet@SysP.myguest.virtualbox.org>
2023-03-21 13:24:17 -0400

    Created directory structure for assignment and makefiles for server and client.

---
+++ CHAT-SYSTEM/chat-client/src/chat-client.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello from chat-client.c\n");
+	return 0;
+}
---
+++ CHAT-SYSTEM/chat-server/src/chat-server.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello from chat-server.c\n");
+	return 0;
+}
